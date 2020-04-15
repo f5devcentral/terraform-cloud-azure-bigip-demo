882596d8689be64feeb1a5883d6c5a194a9719e6
Mark Menger <m.menger@f5.com>
2020-04-15 18:29:05 +0000

    look in terraform state for management ips and password

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,3 @@
-terraform output --json > inspec/bigip-ready/files/terraform.json
+terraform show -json | jq '.values.root_module.resources[] | select(.address | contains("random_password.bigippassword"))  | .values.result' > inspec/bigip-ready/files/password.json
+terraform show -json | jq '.values.root_module.resources[] | select(.address | contains("azurerm_public_ip.management_public_ip"))  | .values.ip_address ' > inspec/bigip-ready/files/mgmtips.json
 inspec exec inspec/bigip-ready
